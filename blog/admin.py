a45ac61ef34017491d4f333bd9fc306d0cf36f3d
zhouhaihai <781184096@qq.com>
2018-02-03 14:04:13 +0800

    修改admin展示

--- blog/admin.py
+++ blog/admin.py
@@ -6,11 +6,23 @@ from .forms import BlogPostForm
 class PostAdmin(admin.ModelAdmin):
     form = BlogPostForm
     filter_horizontal = ('tags',)
+    list_filter = ('title',)
+    exclude = ('author',)
     list_display = ('title', 'created_time', 'pub_date', 'modified_time', 'category', 'author',
                     'views', 'status'
                     )
+    def save_model(self, request, obj, form, change):
+    	if not change :
+    		obj.author = request.user
+    	super().save_model(request, obj, form, change)
 class CategoryAdmin(admin.ModelAdmin):
-	list_display = ('name', 'slug', 'genre', 'status', 'cover', 'creator')
+	exclude = ('creator','resource')
+	list_display = ('name', 'slug', 'genre', 'creator', 'status', 'cover')
+	def save_model(self, request, obj, form, change):
+		print(change)
+		if not change :
+			obj.creator = request.user
+		super().save_model(request, obj, form, change)
 class ImgAdmin(admin.ModelAdmin):
 	list_display = ('name', 'head_pic', 'created')
 
